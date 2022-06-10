6f0129ccdb20ad7f4798236bec0db5c5ce172e11
Parth Desai <desaiparth2000@gmail.com>
2022-06-10 12:59:42 +0530

    Added hover animations and rough animations to index

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
+import { RoughNotation } from 'react-rough-notation'
 
 import NewsletterForm from '@/components/NewsletterForm'
 import Image from '@/components/Image'
@@ -24,15 +25,29 @@ export default function Home({ posts }) {
         <div className="mb-12 flex flex-col items-center gap-x-12 xl:flex-row">
           <div className="pt-6">
             <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Hi, I am Parth Desai.
+              Hi, I am{' '}
+              <span className="text-primary-color dark:text-primary-color-dark">Parth</span>
             </h1>
             <h2 className="prose text-lg text-gray-600 dark:text-gray-300">
               {`Welcome to ${siteMetadata.description}. I am a Data Engineer who is passionate about Data Science and Automation. In my free time, I like developing `}
               <Link href="/projects">side projects</Link>
               {' and '}
               <Link href="/blog">blogging</Link>
-              {' about them. Have a good read!'}
+              {' about them.'}
             </h2>
+            <p className="pt-3 text-lg leading-7 text-slate-600 dark:text-slate-300">
+              This is my place for{' '}
+              <RoughNotation
+                type="box"
+                show={true}
+                color="cyan"
+                animationDelay={800}
+                animationDuration={1200}
+              >
+                thoughts, reflections & everything&nbsp;
+              </RoughNotation>
+              in between. Have a good read!
+            </p>
           </div>
           <div className="flex items-center justify-center">
             <div className="my-2 flex flex-col items-center space-x-2 rounded-full border-2 border-gray-200 border-opacity-60 pt-8 hover:border-primary-600 dark:border-gray-800 dark:hover:border-primary-400">
