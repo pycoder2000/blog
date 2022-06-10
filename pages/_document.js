d883ef4151e7b8b32624a1c8701d21198d9ee9a0
Parth Desai <desaiparth2000@gmail.com>
2022-06-11 01:45:22 +0530

    Added Favicons for all sizes

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,51 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/static/favicons/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="57x57"
+            href="/static/favicons/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="114x114"
+            href="/static/favicons/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="72x72"
+            href="/static/favicons/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="144x144"
+            href="/static/favicons/apple-touch-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="60x60"
+            href="/static/favicons/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="120x120"
+            href="/static/favicons/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="76x76"
+            href="/static/favicons/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="152x152"
+            href="/static/favicons/apple-touch-icon-152x152.png"
+          />
           <link
             rel="icon"
             type="image/png"
@@ -17,10 +61,44 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
           />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/favicons/favicon-196x196.png"
+            sizes="196x196"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/favicons/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/favicons/favicon-128.png"
+            sizes="128x128"
+          />
+          <meta name="msapplication-TileImage" content="/static/favicons/mstile-144x144.png" />
+          <meta name="msapplication-square70x70logo" content="/static/favicons/mstile-70x70.png" />
+          <meta
+            name="msapplication-square150x150logo"
+            content="/static/favicons/mstile-150x150.png"
+          />
+          <meta
+            name="msapplication-wide310x150logo"
+            content="/static/favicons/mstile-310x150.png"
+          />
+          <meta
+            name="msapplication-square310x310logo"
+            content="/static/favicons/mstile-310x310.png"
+          />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="msapplication-TileColor" content="#FFFFFF" />
           <meta name="theme-color" content="#ffffff" />
+          <meta name="description" content="Parth Desai's Blog" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
