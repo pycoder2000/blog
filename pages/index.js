3c1d90d4a0831e9387b544e61ad56215a6bc988e
Parth Desai <desaiparth2000@gmail.com>
2022-06-25 22:41:15 +0530

    Minor change

--- pages/index.js
+++ pages/index.js
@@ -168,7 +168,9 @@ export default function Home({ posts }) {
                             d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                           />
                         </svg>
-                        <span className="pr-6 text-gray-900 dark:text-gray-100">Hire me!</span>
+                        <span className="pr-6 text-gray-900 dark:text-gray-100">
+                          Hire me!&nbsp;&nbsp;&nbsp;
+                        </span>
                       </span>
                       <span className="pl-6 text-primary-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
                         Resume&nbsp;&rarr;
