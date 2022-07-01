f3121caae4a2ea85d42a4d288d47996ab2e6096a
Parth Desai <desaiparth2000@gmail.com>
2022-07-01 15:06:51 +0530

    Minor changes

--- components/ViewCounter.js
+++ components/ViewCounter.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 import useSWR from 'swr'
-import fetcher from 'lib/fetcher'
+import fetcher from '@/lib/fetcher'
 
-export default function ViewCounter({ slug, className }) {
+const ViewCounter = ({ slug, className }) => {
   let { data } = useSWR(`/api/views/${slug}`, fetcher)
   let views = new Number(data?.total)
 
@@ -17,3 +17,5 @@ export default function ViewCounter({ slug, className }) {
 
   return <span className={className}>{`${views > 0 ? views.toLocaleString() : '–––'} views`}</span>
 }
+
+export default ViewCounter
