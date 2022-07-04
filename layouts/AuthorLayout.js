76c2c6593aa0a21a7f59acc822148bde858d3e32
Parth Desai <desaiparth2000@gmail.com>
2022-07-04 18:30:12 +0530

    Minor design bug fixes

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -25,7 +25,7 @@ export default function AuthorLayout({ children, frontMatter }) {
     <>
       <PageSEO title={`About - ${name}`} description={`A little trivia me`} />
       <div className="">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5 md:pl-16">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             About
           </h1>
@@ -240,7 +240,7 @@ export default function AuthorLayout({ children, frontMatter }) {
             </p>
           </div>
         </div>
-        <div className="mt-10">
+        <div className="mt-10 md:pl-16">
           <div className="space-y-2 pt-6 pb-8 md:space-y-5">
             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
               Experience
