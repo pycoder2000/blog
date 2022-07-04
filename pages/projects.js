8434f1926f85d1c3a55ec71e9eda41cea446c862
Parth Desai <desaiparth2000@gmail.com>
2022-07-04 13:08:31 +0530

    Minor SEO changes

--- pages/projects.js
+++ pages/projects.js
@@ -6,7 +6,10 @@ import { PageSEO } from '@/components/SEO'
 export default function Projects() {
   return (
     <>
-      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <PageSEO
+        title={`Projects - ${siteMetadata.author}`}
+        description="A list of projects I have built"
+      />
       <div className="mx-auto max-w-6xl divide-y divide-gray-400">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
