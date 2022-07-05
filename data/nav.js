c1276a47fbc9a3ec4ae58496b60f31b1a69e9126
Parth Desai <desaiparth2000@gmail.com>
2022-07-05 15:41:19 +0530

    Added Command Palette

---
+++ data/nav.js
@@ -0,0 +1,46 @@
+export const navigation = {
+  pages: [
+    { name: 'Home', href: '/' },
+    { name: 'Blog', href: '/blog' },
+    { name: 'Now', href: '/now' },
+    { name: 'About', href: '/about' },
+    { name: 'Stats', href: '/stats' },
+    { name: 'Guestbook', href: '/guestbook' },
+    { name: 'Bookmarks', href: '/bookmarks' },
+    { name: 'Snippets', href: '/snippets' },
+    { name: 'Uses', href: '/uses' },
+    { name: 'Quotes', href: '/quotes' },
+    { name: 'Projects', href: '/projects' },
+    { name: 'Contact', href: '/contact' },
+    { name: 'Todo', href: '/todo' },
+    { name: 'Music', href: '/music' },
+    { name: 'Movies', href: '/movies' },
+  ],
+  footerLinks: [
+    { name: 'Guestbook', href: 'guestbook' },
+    { name: 'Blog', href: '/blog' },
+    { name: 'Snippets', href: '/snippets' },
+  ],
+  navData: [
+    {
+      path: '/',
+      label: 'home',
+    },
+    {
+      path: '/about',
+      label: 'about',
+    },
+    {
+      path: '/now',
+      label: 'now',
+    },
+    {
+      path: '/blog',
+      label: 'blog',
+    },
+    {
+      path: '/guestbook',
+      label: 'guestbook',
+    },
+  ],
+}
