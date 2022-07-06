c4a0a13906a051e2684a2fab927f3f436a5779b4
Parth Desai <desaiparth2000@gmail.com>
2022-07-06 18:30:50 +0530

    Fixed bugs

--- layouts/ActivityLayout.js
+++ layouts/ActivityLayout.js
@@ -1,10 +1,11 @@
 import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
 
 export default function ActivityLayout({ children, frontMatter }) {
   const { name } = frontMatter
   return (
     <>
-      <PageSEO title={`Activity - ${name}`} description="My activities" />
+      <PageSEO title={`Activity - ${siteMetadata.author}`} description="My activities" />
       <div className="">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
