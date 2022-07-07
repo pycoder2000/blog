56d4e28987e22d40f3ac2030b68f2e938d643dad
Parth Desai <desaiparth2000@gmail.com>
2022-07-07 12:33:44 +0530

    Changed music playing animation

--- components/NowPlaying.js
+++ components/NowPlaying.js
@@ -15,7 +15,7 @@ function AnimatedBars() {
         ],
       },
       {
-        duration: 1.0,
+        duration: 0.5,
         repeat: Infinity,
         easing: ['ease-in-out'],
       }
@@ -31,7 +31,7 @@ function AnimatedBars() {
       },
       {
         delay: 0.2,
-        duration: 1.5,
+        duration: 0.5,
         repeat: Infinity,
         easing: ['ease-in-out'],
       }
@@ -47,7 +47,23 @@ function AnimatedBars() {
       },
       {
         delay: 0.3,
-        duration: 1.5,
+        duration: 0.5,
+        repeat: Infinity,
+        easing: ['ease-in-out'],
+      }
+    )
+    animate(
+      '#bar4',
+      {
+        transform: [
+          'scaleY(1.0) translateY(0rem)',
+          'scaleY(1.5) translateY(-0.082rem)',
+          'scaleY(1.0) translateY(0rem)',
+        ],
+      },
+      {
+        delay: 0.1,
+        duration: 0.5,
         repeat: Infinity,
         easing: ['ease-in-out'],
       }
@@ -56,9 +72,10 @@ function AnimatedBars() {
 
   return (
     <div className="flex w-auto items-end overflow-hidden">
-      <span id="bar1" className="mr-[3px] h-2 w-1 bg-gray-300 opacity-75 dark:bg-gray-500" />
-      <span id="bar2" className="mr-[3px] h-1 w-1 bg-gray-300 dark:bg-gray-500" />
-      <span id="bar3" className="h-3 w-1 bg-gray-300 opacity-80 dark:bg-gray-500" />
+      <span id="bar1" className="mr-[3px] h-2 w-0.5 bg-[#1bd760] opacity-75" />
+      <span id="bar2" className="mr-[3px] h-1 w-0.5 bg-[#1bd760]" />
+      <span id="bar3" className="mr-[3px] h-3 w-0.5 bg-[#1bd760] opacity-80" />
+      <span id="bar4" className="mr-[3px] h-2 w-0.5 bg-[#1bd760] opacity-80" />
     </div>
   )
 }
