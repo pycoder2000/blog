0b1be73da14a864061d4cab2f7d49ee89ac33f80
Parth Desai <desaiparth2000@gmail.com>
2022-07-12 15:57:09 +0530

    Updated 404 Page

--- pages/404.js
+++ pages/404.js
@@ -8,23 +8,32 @@ export default function FourZeroFour() {
       <PageSEO title={`Page Not Found - ${siteMetadata.title}`} />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:px-3 md:text-8xl md:leading-14">
             404
           </h1>
         </div>
-        <div className="max-w-md">
-          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
-          </p>
-          <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
-          </p>
-          <Link href="/">
-            <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-              Back to homepage
-            </button>
-          </Link>
+        <div className="mx-auto max-w-2xl">
+          <div className="md:border-l-2 md:px-6">
+            <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+              Unavailable For Legal Reasons
+            </p>
+            <p className="mb-4">
+              Why show a generic 404 when I can make it sound mysterious? It seems you've found
+              something that used to exist, or you spelled something wrong. I'm guessing you spelled
+              something wrong. Can you double check that URL?
+            </p>
+            <p>But dont worry, you can find plenty of other things on our homepage.</p>
+          </div>
         </div>
+      </div>
+      <div className="mt-16 grid place-items-center">
+        <Link href="/">
+          <button aria-label="Scroll To Top" type="button" className="pushable">
+            <span className="shadow"></span>
+            <span className="edgeblue"></span>
+            <span className="frontblue">Return to Homepage</span>
+          </button>
+        </Link>
       </div>
     </>
   )
