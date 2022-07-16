c67e2f131b56528cc080d579e6985589574a89c0
Parth Desai <desaiparth2000@gmail.com>
2022-07-17 01:50:00 +0530

    Updated now page

---
+++ components/DayJS.js
@@ -0,0 +1,9 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Asia/Kolkata')
+
+export { dayjs }
--- pages/now.js
+++ pages/now.js
@@ -1,27 +1,81 @@
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
+import { dayjs } from '@/components/DayJS'
+import { useEffect, useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 import { getCurrentlyReading } from '@/lib/goodreads'
 import fetcher from 'lib/fetcher'
 import useSWR from 'swr'
+import { FaCloudShowersHeavy } from 'react-icons/fa'
+import {
+  BsCloudDrizzleFill,
+  BsCloudsFill,
+  BsCloudLightningFill,
+  BsCloudSnowFill,
+  BsCloudFogFill,
+  BsMoonFill,
+  BsClock,
+  BsSunFill,
+  BsFillCloudSunFill,
+  BsFillCloudMoonFill,
+  BsFillCloudFill,
+} from 'react-icons/bs'
 
-export async function getStaticProps() {
+export const getServerSideProps = async () => {
+  const response = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?lat=23.014770&lon=72.526330&appid=1b3c10c18e894eaf1fd63eedde53fa54&units=metric'
+  )
+  const data = await response.json()
+
   const currentlyReading = await getCurrentlyReading({ limit: 1 })
 
-  return { props: { currentlyReading } }
+  return {
+    props: { currentlyReading, data },
+  }
 }
 
 export default function Now(currentlyReading) {
   const { data } = useSWR('/api/now-playing', fetcher)
   let currentlyReadingData = currentlyReading['currentlyReading']
+  let weatherData = currentlyReading['data']
+  const { temp: temperature } = weatherData.main
+  const { icon: weatherIcon, description: weatherDescription } = weatherData.weather[0]
+
+  const icons = {
+    _01d: <BsSunFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _01n: <BsMoonFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _02d: <BsFillCloudSunFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _02n: <BsFillCloudMoonFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _03d: <BsFillCloudFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _03n: <BsFillCloudFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _04d: <BsCloudsFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _04n: <BsCloudsFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _09d: <BsCloudDrizzleFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _09n: <BsCloudDrizzleFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _10d: <FaCloudShowersHeavy className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _10n: <FaCloudShowersHeavy className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _11d: <BsCloudLightningFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _11n: <BsCloudLightningFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _13d: <BsCloudSnowFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _13n: <BsCloudSnowFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _50d: <BsCloudFogFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+    _50n: <BsCloudFogFill className="mb-0.5 inline h-3 w-3 hover:animate-spin" />,
+  }
 
   var year = new Date().getFullYear()
   var month = new Date().getMonth()
   var date = new Date().getDate()
-  var time = new Date().getTime()
   var hour = new Date().getHours()
   var minute = new Date().getMinutes()
   var second = new Date().getSeconds()
+  const now = () => dayjs().tz()
+  const format = 'hhA'
+  const [TodayDate, setDate] = useState(now())
+
+  useEffect(() => {
+    const timer = setInterval(() => setDate(now()), 1000)
+    return () => clearInterval(timer)
+  }, [])
 
   var ParthBirthDate = '2000-04-16'
   var birthDate = new Date(ParthBirthDate)
@@ -55,19 +109,18 @@ export default function Now(currentlyReading) {
 
   var ageString = ''
   if (age.years > 0 && age.months > 0 && age.days > 0)
-    ageString = age.years + ' years, ' + age.months + ' months, and ' + age.days + ' days old.'
+    ageString = age.years + ' years, ' + age.months + ' months, and ' + age.days + ' days old'
   else if (age.years == 0 && age.months == 0 && age.days > 0)
-    ageString = 'Only ' + age.days + ' days old!'
+    ageString = 'Only ' + age.days + ' days old'
   else if (age.years > 0 && age.months == 0 && age.days == 0)
     ageString = age.years + ' years old. Happy Birthday!!'
   else if (age.years > 0 && age.months > 0 && age.days == 0)
-    ageString = age.years + ' years and ' + age.months + ' months old.'
+    ageString = age.years + ' years and ' + age.months + ' months old'
   else if (age.years == 0 && age.months > 0 && age.days > 0)
-    ageString = age.months + ' months and ' + age.days + ' days old.'
+    ageString = age.months + ' months and ' + age.days + ' days old'
   else if (age.years > 0 && age.months == 0 && age.days > 0)
-    ageString = age.years + ' years, and' + age.days + ' days old.'
-  else if (age.years == 0 && age.months > 0 && age.days == 0)
-    ageString = age.months + ' months old.'
+    ageString = age.years + ' years, and' + age.days + ' days old'
+  else if (age.years == 0 && age.months > 0 && age.days == 0) ageString = age.months + ' months old'
   else ageString = "Welcome to Earth! <br> It's first day on Earth!"
 
   return (
@@ -80,52 +133,82 @@ export default function Now(currentlyReading) {
       <div>
         <div className="my-2">
           <h3>Where am I and what am I doing?</h3>
-          <div className=" mt-4 mb-8 text-xs text-neutral-700 dark:text-neutral-400">
+          <div className=" mt-4 mb-6 text-xs text-neutral-700 dark:text-neutral-400">
             This page was automatically updated @ {date}-{month}-{year} {hour}:{minute}:{second}
           </div>
         </div>
         {/* Misc */}
-        <div className="flex justify-between">
-          <div className="mt-2 mb-10 w-1/4 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
-            <span className="ml-2 font-semibold">Location:</span> <span>Ahmedabad</span>
-            <br />
-            <span className="ml-2 font-semibold">Weather:</span> <span>31°C, extreme rain</span>
-          </div>
-
-          <div className="mt-2 mb-10 w-2/5 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
-            <span className="ml-2 font-semibold">Reading:</span>{' '}
-            <a
-              href={currentlyReadingData[0].url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:underline"
-            >
-              <span>{currentlyReadingData[0].title}</span> by{' '}
-              <span>{currentlyReadingData[0].author}</span>
-            </a>
-            <br />
-            <span className="ml-2 font-semibold">Age:</span> <span>{ageString}</span>
-          </div>
-
-          <div className="mt-2 mb-10 w-1/4 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
-            <span className="ml-2 font-semibold">Listening:</span>{' '}
-            <span>
-              {data?.songUrl ? (
+        <div>
+          <div className="flex justify-between gap-5">
+            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Location:</span> <span>Ahmedabad, India</span>
+              <br />
+              <span className="ml-2 font-semibold">Weather:</span>{' '}
+              <span>
                 <a
-                  href={data.songUrl}
+                  href="https://weather.com/en-GB/weather/today/l/f42d9f8baa19b4d8d5e034449faa703839993366f64551a56a2b530297075dc2"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="hover:underline"
+                  className="underline-offset-1 hover:underline"
                 >
-                  <span>{data.title}</span>
+                  {icons[`_${weatherIcon}`]} Currently <b>{parseInt(temperature)}°C</b>
+                  {' with '}
+                  <span>{weatherDescription}</span>
                 </a>
-              ) : (
-                <span>Not Playing</span>
-              )}
-            </span>
-            <br />
-            <span className="ml-2 font-semibold">Drinking:</span> <span>Mango Milkshake</span>
+              </span>
+            </div>
+
+            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Reading:</span>{' '}
+              <a
+                href={currentlyReadingData[0].url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline-offset-1 hover:underline"
+              >
+                <span>{currentlyReadingData[0].title}</span> by{' '}
+                <span>{currentlyReadingData[0].author}</span>
+              </a>
+              <br />
+              <span className="ml-2 font-semibold">Age:</span> <span>{ageString}</span>
+            </div>
           </div>
+
+          <div className="-my-6 flex justify-between gap-5">
+            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Date:</span>{' '}
+              <span>{TodayDate.format('DD/MM/YYYY')}</span>
+              <br />
+              <span className="ml-2 font-semibold">Time:</span>{' '}
+              <span>
+                <BsClock className="mb-0.5 inline h-3 w-3 hover:animate-spin" />{' '}
+                {TodayDate.format('h:mm:ss A')}
+              </span>
+            </div>
+
+            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Listening:</span>{' '}
+              <span>
+                {data?.songUrl ? (
+                  <a
+                    href={data.songUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline-offset-1 hover:underline"
+                  >
+                    <span>{data.title}</span>
+                  </a>
+                ) : (
+                  <span>Not Playing</span>
+                )}
+              </span>
+              <br />
+              <span className="ml-2 font-semibold">Drinking:</span> <span>Coffee</span>
+            </div>
+          </div>
+        </div>
+        <div className="justify-center text-center text-2xl font-medium text-gray-200 dark:text-gray-600">
+          &#126;&#126;&#126;
         </div>
         {/* Work */}
         <div className="pb-4">
