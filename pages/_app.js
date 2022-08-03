123e9c87c6bea7e3b2bfc7c723b9fb576f521641
Parth Desai <desaiparth2000@gmail.com>
2022-08-03 22:23:17 +0530

    Secured Lyket API Key

--- pages/_app.js
+++ pages/_app.js
@@ -53,7 +53,7 @@ const defaultTheme = {
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Provider apiKey="pt_7c8b6840f5ba39cd3b2b471cd8efc2" theme={defaultTheme}>
+      <Provider apiKey={process.env.LYKET_API_KEY} theme={defaultTheme}>
         <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
           <ProgressBar bgcolor="#DE1D8D" />
           <ScrollTop />
