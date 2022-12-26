878b57c1ab1377aa432d1e716133109e100cf59c
pramit-marattha <pramitmarattha5497@gmail.com>
2022-12-26 10:52:55 +0545

    Update contact info links and about page

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -82,9 +82,9 @@ export default function AuthorLayout({ children, frontMatter }) {
               related to
               <span className="font-semibold">
                 {' '}
-                Natural Language Processing and Machine Learning.{' '}
+                DataScience + DataOps + BI {' '}
               </span>
-              At the same time I am{' '}
+             
               <RoughNotation
                 type="underline"
                 show={true}
@@ -93,45 +93,15 @@ export default function AuthorLayout({ children, frontMatter }) {
                 animationDuration={3000}
                 multiline={true}
               >
-                actively on the lookout for remote internships which I can pursue in field of Data
-                Science.
-              </RoughNotation>
-            </p>
-            <p className="hidden md:block">
-              I am always looking to learn new things. I am currently working on a few projects
-              related to{' '}
-              <RoughNotation
-                animationDelay="1000"
-                animationDuration="3000"
-                type="highlight"
-                color="#0ea4e9"
-                strokeWidth="3"
-                show={true}
-              >
-                <span className="text-black dark:text-white">
-                  Natural Language Processing and Machine Learning.{' '}
-                </span>
-              </RoughNotation>
-              At the same time I am{' '}
-              <RoughNotation
-                type="underline"
-                show={true}
-                color="#FBCFE8"
-                animationDelay={1500}
-                animationDuration={3000}
-                multiline={true}
-              >
-                actively on the lookout for remote internships which I can pursue in field of Data
-                Science
+              
               </RoughNotation>
             </p>
             <br />
             <p>
-              I am a strong advocate for open source and I am always interested in working on new
-              projects with new people. Feel free to reach out if you have anything to talk about,
-              you can reach me through{' '}
+            I strongly support the use of open source software and am always eager to collaborate on new projects with different people. 
+            Please don't hesitate to contact me if you want to discuss anything.
               <Link
-                href={'mailto:desaiparth2000@gmail.com'}
+                href={'mailto:pramitmarattha5497@gmail.com'}
                 className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
               >
                 Mail
@@ -149,92 +119,8 @@ export default function AuthorLayout({ children, frontMatter }) {
                   </g>
                 </svg>
               </Link>{' '}
-              or{' '}
-              <Link
-                href={'https://api.whatsapp.com/send?phone=916358190818&text=hi'}
-                className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-              >
-                Whatsapp
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  className="ml-0.5 inline-block h-4 w-4 fill-current"
-                >
-                  <g data-name="Layer 2">
-                    <g data-name="external-link">
-                      <rect width="24" height="24" opacity="0" />
-                      <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
-                      <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
-                    </g>
-                  </g>
-                </svg>
-              </Link>
             </p>
             <br />
-            <p>
-              <Link
-                href={'/uses'}
-                className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-              >
-                Here
-              </Link>{' '}
-              you can see what I use on daily basis
-            </p>
-            <br />
-            <h1>About this site</h1>
-            <p>
-              Welcome to my home on the internet. This site functions as a blog/portfolio, a place
-              to share code and thoughts. Opinions are my own.
-            </p>
-            <p>
-              I learnt how to build this site from the most awesome people in the community:
-              <ul>
-                <li>
-                  <Link
-                    href={'https://github.com/timlrx/tailwind-nextjs-starter-blog'}
-                    className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-                  >
-                    Timothy's Next.js and Tailwind CSS template
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      className="ml-0.5 inline-block h-4 w-4 fill-current"
-                    >
-                      <g data-name="Layer 2">
-                        <g data-name="external-link">
-                          <rect width="24" height="24" opacity="0" />
-                          <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
-                          <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
-                        </g>
-                      </g>
-                    </svg>
-                  </Link>
-                  : Template starter where I bootstrapped the project.
-                </li>
-                <li>
-                  <Link
-                    href={'https://www.einargudni.com/'}
-                    className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-                  >
-                    Einar Guðjónsson
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      className="ml-0.5 inline-block h-4 w-4 fill-current"
-                    >
-                      <g data-name="Layer 2">
-                        <g data-name="external-link">
-                          <rect width="24" height="24" opacity="0" />
-                          <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
-                          <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
-                        </g>
-                      </g>
-                    </svg>
-                  </Link>
-                  : Now page, navigation style, animations and much more.
-                </li>
-              </ul>
-            </p>
           </div>
         </div>
         <div className="mt-10 md:pl-16">
