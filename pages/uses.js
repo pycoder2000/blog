11be1036a5dcdba988a9871fc33a6f82e4459676
pramit-marattha <pramitmarattha5497@gmail.com>
2022-12-26 10:05:08 +0545

    Fix @/data/usesData module not found error

--- pages/uses.js
+++ pages/uses.js
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import { PageSEO } from '@/components/SEO'
-import usesData from '@/data/usesData'
 import Link from '@/components/Link'
 
 export default function Uses() {
@@ -22,40 +21,6 @@ export default function Uses() {
         </div>
         <div className="container py-12">
           <div className="flex flex-row flex-wrap">
-            {usesData.map((d) => (
-              <Link
-                key={d.href}
-                href={d.href}
-                className="group mb-4 w-full cursor-pointer rounded-xl p-6 backdrop-filter transition duration-100 hover:scale-[1.02] hover:bg-gray-300 hover:bg-opacity-40 dark:hover:bg-gray-500 dark:hover:bg-opacity-40 md:w-1/2"
-              >
-                <div className="flex items-center justify-start">
-                  <div className="flex-shrink-0 p-3 font-sans text-gray-700 dark:text-gray-50 ">
-                    <svg
-                      width={36}
-                      height={36}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="h-12 w-12 stroke-current text-center align-middle leading-6 text-gray-700 dark:text-gray-50"
-                    >
-                      <path d="M4 17L10 11 4 5" />
-                      <path d="M12 19L20 19" />
-                    </svg>
-                  </div>
-                  <div className="flex flex-col p-3">
-                    <h3 className="truncate text-sm font-bold leading-5 text-gray-800 dark:text-white sm:text-base lg:text-base">
-                      {d.title}
-                    </h3>
-                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 sm:text-base lg:text-sm xl:text-base">
-                      {d.description}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            ))}
           </div>
         </div>
         <div className="pt-10">
