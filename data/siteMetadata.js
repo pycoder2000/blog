364b2a1dc9cb92310d18f72ea8055e803c8d828a
focus85 <henokshiferaw@me.com>
2022-12-30 20:22:52 -0500

    some update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,12 +17,12 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com/in/hshiferaw/',
   website: 'https://parthdesai.vercel.app',
   locale: 'en-US',
-  //analytics: {
-  //  plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-  //  simpleAnalytics: false, // true or false
-  //  umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //  googleAnalyticsId: 'G-F6V2QTJ628', // e.g. UA-000000-2 or G-XXXXXXX
-  //},
+  analytics: {
+    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: 'G-F6V2QTJ628', // e.g. UA-000000-2 or G-XXXXXXX
+  },
   newsletter: {
     provider: 'emailOctopus',
   },
