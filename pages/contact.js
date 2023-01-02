d00270d9870f582048059edfa3d40987ffb979d1
focus85 <henokshiferaw@me.com>
2023-01-01 20:42:43 -0500

    update stats

--- pages/contact.js
+++ pages/contact.js
@@ -12,17 +12,13 @@ const Contact = () => {
         </h1>
         <div className="pt-10 pb-8">
           <ul className="font-semi-bold flex flex-col space-y-4">
+            <ContactLink href="mailto:henok@hshiferaw.com" title="E-mail" icon="henok" />
+            <ContactLink href="https://github.com/pycoder2000" title="github" icon="hshiferaw" />
+            <ContactLink href="https://twitter.com/lone_Musk" title="twitter" icon="meaty_pirate" />
             <ContactLink
-              href="mailto:desaiparth2000@gmail.com"
-              title="gmail"
-              icon="desaiparth2000"
-            />
-            <ContactLink href="https://github.com/pycoder2000" title="github" icon="pycoder2000" />
-            <ContactLink href="https://twitter.com/lone_Musk" title="twitter" icon="lone_Musk" />
-            <ContactLink
-              href="https://www.linkedin.com/in/parth-desai-2bb1b0160/"
+              href="https://www.linkedin.com/in/hshiferaw/"
               title="linkedin"
-              icon="parthdesai"
+              icon="henok"
             />
           </ul>
         </div>
