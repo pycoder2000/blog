412581dff9dc0701ce480ea6e9c3d7f1b5b96175
focus85 <henokshiferaw@me.com>
2023-01-02 15:20:06 -0500

    update timeline

--- components/Timeline.js
+++ components/Timeline.js
@@ -36,6 +36,9 @@ export default function Timeline() {
           </span>
           <h3 className="text-base font-semibold text-gray-900 dark:text-white">
             Joined PhD Program
+            <span className="mr-2 ml-3 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-200 dark:text-blue-800">
+              Present
+            </span>
           </h3>
           <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             January, 2012
