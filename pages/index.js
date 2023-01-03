2614ca95fe733c0b60761f2b17223cedb36eb432
focus85 <henokshiferaw@me.com>
2023-01-03 11:38:25 -0500

    update index page

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ posts }) {
           <div className="pt-6">
             <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
               Hi, I am{' '}
-              <span className="text-primary-color dark:text-primary-color-dark">Henok</span>
+              <span className="text-primary-color-400 dark:text-primary-color-dark-400">Henok</span>
             </h1>
             <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-300">
               {`Welcome to ${siteMetadata.description}. I am a Patent Examiner in electrical and software engineering, who is passionate about Data Science and Automation. In my free time, I like developing `}
