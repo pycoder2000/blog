a93d29d1942328d64bab73f3c5098f20330fb984
focus85 <henokshiferaw@me.com>
2023-01-05 15:27:22 -0500

    update index page

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react'
 
 import siteMetadata from '@/data/siteMetadata'
 
-const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
+const NewsletterForm = ({ title = 'Subscribe to get an update' }) => {
   const inputEl = useRef(null)
   const [error, setError] = useState(false)
   const [message, setMessage] = useState('')
